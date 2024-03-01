06277423a3a3f863e0574c442bec620e4531e839
Solomon Odunusi <solomonodunusi@gmail.com>
2024-03-01 02:07:32 +0100

    Debugging SessionProvider

--- provider/AuthProvider.jsx
+++ provider/AuthProvider.jsx
@@ -4,7 +4,7 @@ import {SessionProvider} from 'next-auth/react'
 
 const AuthProvider = ({children}) => {
   return (
-    <SessionProvider basePath="/utils/auth">{children}</SessionProvider>
+    <SessionProvider>{children}</SessionProvider>
   )
 }
 
